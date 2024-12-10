ed05310909f024b709060da6af5432e2b63150bd
alan cruz <alan_mugiwara@hotmail.com>
2024-12-09 23:26:08 -0300

    Inclusão da página de convidados

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const listaConvidados = document.getElementById('lista-convidados');
+    const confirmacoes = document.getElementById('Confirmações');
+  
+    // Função para atualizar o contador de confirmações
+    function atualizarContador() {
+      let count = 0;
+      const checkboxes = listaConvidados.querySelectorAll('input[type="checkbox"]:checked');
+      count = checkboxes.length;
+      confirmacoes.textContent = `Confirmações: ${count}`;
+    }
+  
+    // Adiciona o event listener para atualizar o contador ao mudar o estado dos checkboxes
+    listaConvidados.addEventListener('change', atualizarContador);
+  
+    // Chama a função para atualizar o contador ao carregar a página
+    atualizarContador(); 
+});
